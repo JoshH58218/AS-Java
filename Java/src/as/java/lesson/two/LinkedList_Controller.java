0a50ecf61eb13383667f320d2e2d8ce7cde8a726
JoshH58218 <jhill.58218@farnborough.ac.uk>
2015-12-03 16:20:51 +0000

    03/12/15 Edit 1

---
+++ Java/src/as/java/lesson/two/LinkedList_Controller.java
@@ -0,0 +1,37 @@
+package as.java.lesson.two;
+import java.util.Scanner;
+public class LinkedList_Controller {
+	public static void main(String[]args){
+		Scanner sc=new Scanner (System.in);
+		Linked_List studentDataList=new Linked_List();
+		
+		String command;
+		for(;;){
+			System.out.println("Input a command (Add at front, Delete at front, Display list, Number of links, or End");
+			command=sc.next();
+			if(command.equals("AddAtFront")){
+				
+				studentDataList.insertFirst(sc.next(), sc.nextInt(), sc.next(), sc.nextBoolean());
+			}else if(command.equals("DeleteAtFront")){
+				studentDataList.deleteFirst();
+				
+			}else if(command.equals("DisplayList")){
+				studentDataList.displayList();
+			}else if(command=="Number of links"){
+				
+			}else if(command.equals("End")){
+				break;
+			}else{
+				System.out.println("Invalid command");
+				System.out.println();
+			}
+			
+	
+		
+		}
+		sc.close();
+	}
+	
+	
+
+}
--- Java/src/as/java/lesson/two/Linked_List.java
+++ Java/src/as/java/lesson/two/Linked_List.java
@@ -1,7 +1,47 @@
 package as.java.lesson.two;
 
 public class Linked_List {
+	private Link first;
 	
+	public void Linked_List(){
+		first=null;
+	}
+	
+	public boolean isEmpty(){
+		return (first==null);
+	}
+	
+	public void insertFirst(String Name,int age, String favSubject, boolean inTeam){
+		
+		Link link=new Link(Name,age,favSubject,inTeam);
+		
+		if(first!=null){
+			link.setnextLink(first);
+			first=link;
+		}
+		else{
+			first=link;
+		}
+	}
+	
+	public Link deleteFirst(){
+		
+		Link deletedLink=first;
+		first=first.getnextLink();
+		return deletedLink;
+	}
+	
+	public void displayList(){
+		
+		System.out.print("List (first-->last): ");
+		Link current=first;
+		
+		while(current!=null){
+			current.displayLink();
+			current=current.getnextLink();
+		}
+		System.out.println(" ");
+	}
 	
 
 }
